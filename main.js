8b6484ae0aecaec25bb245e91de3b51533921f5c
Uday <jampalauday7@gmail.com>
2022-11-10 21:47:05 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+function main() {
+  document.getElementById('printPdf').addEventListener('click', () => {
+    const inputDiv = document.getElementById('input');
+    const logoDiv = document.getElementById('logo');
+    const detailsDiv = document.getElementById('details');
+    inputDiv.style.display = 'none';
+    logoDiv.style.display = 'inline';
+    detailsDiv.style.display = 'inline';
+    document.getElementById('addItem').setAttribute('hidden', 'hidden');
+    const table = document.getElementById('itemList');
+    // eslint-disable-next-line max-len, no-shadow
+    let sumVal = 0;
+    // eslint-disable-next-line no-plusplus
+    for (let i = 1; i < table.rows.length; i++) {
+      // eslint-disable-next-line no-undef
+      sumVal += parseFloat(table.rows[i].cells[3].innerHTML);
+    }
+    const tbody = document.querySelector('#itemList tbody');
+    const row = `<td colspan="2"></td><td>Total</td><td>${sumVal.toFixed(2)}</td>`;
+    const tr = document.createElement('tr');
+    tr.innerHTML = row;
+    tbody.appendChild(tr);
+  });
+  document.getElementById('addItem').addEventListener('click', () => {
+    let rows = '';
+    // const itemName = document.getElementById('itemName').value;
+    const selector = document.querySelector('#items');
+    const itemName = selector.options[selector.selectedIndex].text;
+    const itemQty = document.getElementById('itemQty').value;
+    const itemPrice = document.getElementById('itemPrice').value;
+    // eslint-disable-next-line no-undef
+    const cost = parseInt(itemQty, 10) * parseInt(itemPrice, 10);
+    rows += `<td>${itemName}</td><td>${itemQty}</td><td>${itemPrice}</td><td>${cost}</td>`;
+    const tbody = document.querySelector('#itemList tbody');
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = rows;
+    tbody.appendChild(tr);
+  });
+}
+
+main();
